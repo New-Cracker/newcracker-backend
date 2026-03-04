968ae63bd06243b1939f951fe55c2a267af5d770
Knhye <knahye311@gmail.com>
2026-03-04 12:05:43 +0900

    feature: db 연결 방식 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmModuleOptions } from './configs/typeorm.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmModuleOptions), UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true, // 개발용만 true
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+    }),
+
+    UserModule,
+  ],
 })
 export class AppModule {}
