cb6d4c858f81742ccdac6299f99baa19a11c9f9e
Knhye <knahye311@gmail.com>
2026-03-04 10:29:26 +0900

    first commit

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false })
+  isDeleted: boolean;
+}
---
+++ src/user/user.repository.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+// import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserRepository {
+  private userRepository: Repository<User>;
+
+  constructor(private readonly datasource: DataSource) {
+    this.userRepository = this.datasource.getRepository(User);
+  }
+
+  // createBoard(createUserDto: CreateUserDto){
+  //   ...
+  //   return this.userRepository.save(user);
+  // }
+}
